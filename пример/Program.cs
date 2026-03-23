49296c4e1d88c0e293f3e025ce64411e842dfb14
Михаил <3301v@mail.ru>
2026-03-23 18:42:45 +0300

    primer

---
+++ пример/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+class mashina
+{
+    private int speed;
+    private string model;
+    private string color;
+
+    public mashina()
+    {
+        speed = 240;
+        model = "mazda";
+        color = "red";
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Модель: {model}, Цвет: {color}, Скорость: {speed} км/ч");
+    }
+
+    static void Main(string[] args)
+    {
+        mashina obj = new mashina();  
+        Console.WriteLine("Объект:"); 
+        obj.PrintInfo(); 
+
+        Console.ReadLine(); 
+    }
+
+}
+ 
